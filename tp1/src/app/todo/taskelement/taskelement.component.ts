b9db301dfc6af7bfd305c1c560672876025671d9
Aurelien RICHE <aurelienriche00@gmail.com>
2020-05-11 16:31:00 +0200

    Add style

--- tp1/src/app/todo/taskelement/taskelement.component.ts
+++ tp1/src/app/todo/taskelement/taskelement.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../interfaces/task.interface';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-taskelement',
   templateUrl: './taskelement.component.html',
-  styleUrls: ['./taskelement.component.sass']
+  styleUrls: ['./taskelement.component.scss']
 })
 export class TaskelementComponent implements OnInit {
 
   @Input() task: Task;
 
-  constructor() { }
+  constructor(
+    private taskService: TodoService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  deleteTodo(id: number): void {
+    this.taskService.delete(id);
+  }
+
+  switchTodoState(id: number): void {
+    this.taskService.switchState(id);
   }
 
 }
