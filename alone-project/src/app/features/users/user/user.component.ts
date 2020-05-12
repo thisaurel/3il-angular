e6c196e8f1b7c1202850a9e2ecf8ee296e76b5e5
Aurélien Riche <contact@aurelienriche.com>
2020-05-12 18:10:40 +0200

    Add user page

--- alone-project/src/app/features/users/user/user.component.ts
+++ alone-project/src/app/features/users/user/user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/interfaces/user';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-user',
@@ -7,7 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  public id: number;
+  public user: User;
+  public mapsURL: string = '';
+
+  constructor(
+    private authService: AuthService,
+    route: ActivatedRoute,
+    public sanitizer: DomSanitizer,
+  ) {
+    const id: Observable<string> = route.params.pipe(map(p => p.id));
+    id.subscribe((routeId) => {
+      this.user = this.authService.getUserById(+routeId);
+      this.mapsURL = `https://maps.google.com/maps?q=${this.user.localisation.lat},${this.user.localisation.long}&hl=es&z=14&output=embed`;
+    
+    });
+  }
 
   ngOnInit() {
   }
